f78ea4d5914cdc8edeb2a6f8058bebe6e3ad203c
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-03-23 08:23:00 -0500

    feat: finalizei a logica do modal para deslogar usuario

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,26 @@ import userIcon from "../assets/userIcon.svg";
 import exitIcon from "../assets/exitIcon.svg";
 import Training from "../components/Training.jsx";
 import { Barbell, ForkKnife } from "@phosphor-icons/react";
-import { useEffect, useState } from "react";
-import { Link, Navigate } from "react-router";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import { verifyToken } from "../services/authServices";
 
 export default function Home() {
-  const [editClass, setEditClass] = useState('')
+  const [editClass, setEditClass] = useState("");
   const [user, setUser] = useState({});
   const [showPage, setShowPage] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const navigate = useNavigate();
   let page = "trainnig";
 
-  useEffect(() => {
-    verifyTokenStorage()
-  }, []);
+  const verifyTokenStorage = useCallback(async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+    const response = await verifyToken(token);
+    handleUserPage(response);
+  }, [navigate]);
 
   function editTrainningHome() {
     if (editClass === "") {
@@ -26,19 +33,27 @@ export default function Home() {
   }
 
   function handleUserPage(response) {
-    setUser(response)
-    setShowPage(true)
+    setUser(response);
+    setShowPage(true);
   }
 
-  async function verifyTokenStorage() {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      return <Navigate to="/login" />;
-    }
-    const response = await verifyToken(token)
-
-    handleUserPage(response)
+  function handleCloseModal() {
+    setOpenModal(false);
   }
+
+  function handleOpenModal() {
+    setOpenModal(!openModal);
+  }
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    setShowPage(false);
+    navigate("/login");
+  }
+
+  useEffect(() => {
+    verifyTokenStorage();
+  }, [showPage, verifyTokenStorage]);
 
   {
     if (!showPage) {
@@ -49,51 +64,87 @@ export default function Home() {
       );
     } else {
       return (
-        <div className="relative w-full h-screen flex flex-col">
-          <header className="w-full h-[120px] bg-gray-02 flex justify-between items-center px-7">
-            <div className="flex items-center justify-center gap-3">
-              <img
-                src={userImg}
-                alt="Imagem do usuario"
-                className="border-2 rounded-4xl border-gray-03"
-              />
-              <p className="font-bold text-xl text-purple">Olá, {user.name.split(" ")[0]}</p>
-            </div>
-            <div className="flex items-center justify-center gap-3">
-              <img src={userIcon} alt="Icone de acesso ao perfil do usuario" />
-              <img src={exitIcon} alt="Icone para sair da conta" />
-            </div>
-          </header>
-          <main
-            className={`w-full h-[calc(100vh-120px)] flex px-6 flex-col items-center ${editClass !== '' ? 'justify-center' : ''} bg-gray-01`}
+        <>
+          <div className="w-full h-screen bg-black fixed inset-0 z-1"></div>
+          <div
+            className={`relative w-full h-screen flex flex-col z-2 ${
+              openModal ? "opacity-20" : "opacity-100"
+            }`}
           >
-            <div
-              className={`${editClass} w-full flex justify-center items-center gap-7 my-5`}
+            <header className="w-full h-[120px] bg-gray-02 flex justify-between items-center px-7">
+              <div className="flex items-center justify-center gap-3">
+                <img
+                  src={userImg}
+                  alt="Imagem do usuario"
+                  className="border-2 rounded-4xl border-gray-03"
+                />
+                <p className="font-bold text-xl text-purple">
+                  Olá, {user.name.split(" ")[0]}
+                </p>
+              </div>
+              <div className="flex items-center justify-center gap-3">
+                <img
+                  src={userIcon}
+                  alt="Icone de acesso ao perfil do usuario"
+                  className="cursor-pointer"
+                />
+                <img
+                  src={exitIcon}
+                  alt="Icone para sair da conta"
+                  className="cursor-pointer"
+                  onClick={handleOpenModal}
+                />
+              </div>
+            </header>
+            <main
+              className={`w-full h-[calc(100vh-120px)] flex px-6 flex-col items-center ${
+                editClass !== "" ? "justify-center" : ""
+              } bg-gray-01`}
             >
-              <Barbell
-                size={60}
-                color={page === "trainnig" ? "#7C7C8A" : "#FFFD54"}
-                weight="regular"
-              />
-              <div className="bg-purple w-1 h-17"></div>
-              <ForkKnife
-                size={60}
-                color={page === "nutrition" ? "#7C7C8A" : "#FFFD54"}
-                weight="regular"
-              />
-            </div>
-            {page === "trainnig" ? (
-              <Training editTrainningHome={editTrainningHome} />
-            ) : null}
-          </main>
-          <div className="w-[340px] h-[125px] bg-gray-02 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md flex flex-col justify-center items-center gap-3">
+              <div
+                className={`${editClass} w-full flex justify-center items-center gap-7 my-5`}
+              >
+                <Barbell
+                  size={60}
+                  color={page === "trainnig" ? "#7C7C8A" : "#FFFD54"}
+                  weight="regular"
+                />
+                <div className="bg-purple w-1 h-17"></div>
+                <ForkKnife
+                  size={60}
+                  color={page === "nutrition" ? "#7C7C8A" : "#FFFD54"}
+                  weight="regular"
+                />
+              </div>
+              {page === "trainnig" ? (
+                <Training editTrainningHome={editTrainningHome} />
+              ) : null}
+            </main>
+          </div>
+          <div
+            className={
+              !openModal
+                ? "hidden"
+                : "w-[340px] h-[125px] bg-gray-02 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md flex flex-col justify-center items-center gap-3 z-3"
+            }
+          >
             <h1 className="font-bold text-white text-2xl">Tem certeza?</h1>
             <div className="w-full flex justify-center items-center gap-3">
-              <button className="w-[130px] h-[30px] rounded-md text-[20px] text-white font-bold bg-purple">Não</button>
-              <button className="w-[130px] h-[30px] rounded-md text-[20px] text-white font-bold bg-gray-03">Sim</button>
+              <button
+                onClick={handleCloseModal}
+                className="w-[130px] h-[30px] rounded-md text-[20px] text-white font-bold bg-purple hover:cursor-pointer"
+              >
+                Não
+              </button>
+              <button
+                onClick={handleLogout}
+                className="w-[130px] h-[30px] rounded-md text-[20px] text-white font-bold bg-gray-03 hover:cursor-pointer"
+              >
+                Sim
+              </button>
             </div>
           </div>
-        </div >
+        </>
       );
     }
   }
