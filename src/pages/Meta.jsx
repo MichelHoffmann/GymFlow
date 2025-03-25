10d43fb4b83e961560e492dac1d554226ab70822
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-03-25 13:03:50 -0500

    feat: criando logica de adicionar meta

--- src/pages/Meta.jsx
+++ src/pages/Meta.jsx
@@ -1,15 +1,21 @@
 import { motion } from "motion/react";
-import { useCallback, useEffect } from "react";
+import { useState } from "react";
 import bgImage from "../assets/bgMeta.png";
+import { addMeta } from "../services/authServices";
 
 export default function Meta() {
-  const fetchUserData = useCallback(async () => {
-    console.log("fetchUserData");
-  }, []);
+  const [meta, setMeta] = useState("");
+  
+  async function handleAddMeta() {
+    const email = localStorage.getItem("email");
+    const response = addMeta({ email, meta });
+    return response;
+  }
 
-  useEffect(() => {
-    fetchUserData();
-  }, [fetchUserData]);
+  function handleChange(e) {
+    const inputValue = Number(e.target.value);
+    setMeta(inputValue);
+  }
   return (
     <motion.div
       initial={{ y: 1000, opacity: 0 }}
@@ -18,7 +24,7 @@ export default function Meta() {
       className="w-full h-screen bg-bottom bg-no-repeat flex justify-center items-center"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className="border-2 border-purple-three rounded-2xl px-[25px] py-[20px] flex flex-col justify-center items-center gap-5">
+      <div className="border-2 border-purple-three bg-purple-three/20 rounded-2xl px-[25px] py-[20px] flex flex-col justify-center items-center gap-5">
         <h1 className="text-xl font-extrabold text-white">
           Defina uma meta <br /> semanal de Km:
         </h1>
@@ -28,12 +34,17 @@ export default function Meta() {
             type="number"
             name="meta"
             id=""
+            value={meta}
+            onChange={handleChange}
           />
           <button className="absolute right-6 top-2 font-extrabold text-purple">
             KM
           </button>
         </div>
-        <button className="rounded-2xl font-extrabold text-purple bg-gray-02 w-40 py-[4px] border-1 border-purple">
+        <button
+          className="rounded-2xl font-extrabold text-purple bg-gray-02 w-40 py-[4px] border-1 border-purple hover:cursor-pointer"
+          onClick={handleAddMeta}
+        >
           Confirmar
         </button>
       </div>
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,7 +3,7 @@ import api from "./apiClient.js";
 export const registerUser = async (userData) => {
   const { name, email, password } = userData;
   try {
-    await api.post('/cadastro', {name, email, password});
+    await api.post("/cadastro", { name, email, password });
     return { success: true, message: "Usuario cadastrado com sucesso!" };
   } catch (error) {
     return { success: false, message: error.response.data.message };
@@ -15,6 +15,7 @@ export const loginUser = async (userData) => {
   try {
     const response = await api.post("/login", { email, password });
     localStorage.setItem("token", response.data.token);
+    localStorage.setItem("email", response.data.user);
     return { success: true, message: response };
   } catch (error) {
     return { success: false, message: error.response.data };
@@ -25,11 +26,36 @@ export const verifyToken = async (token) => {
   try {
     const response = await api.get("/user/me", {
       headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    return response.data
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+export const addMeta = async (userInfo) => {
+  const { email, meta } = userInfo;
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return { success: false, message: "Token não encontrado" };
+  }
+
+  try {
+    const response = await api.patch(
+      "user/me",
+      { email, meta },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(`RESPONSE:: ${response}`);
+  } catch (error) {
+    console.log(error.response.data);
+    return error;
+  }
+};
