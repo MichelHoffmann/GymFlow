2d358e8a48b82e5d7ce9dfa216303928e16ed84c
mic.hoff.jr <mic.hoff.jr@gmail.com>
2025-03-31 13:55:38 +0000

    fix: corrigido erro de importação

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,20 @@ import { Route, Routes, useLocation } from "react-router";
 import Login from "./pages/Login.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
 import Home from "./pages/Home.jsx";
-import { AnimatePresence } from "motion/react";
 import Start from "./pages/Start.jsx";
 import Meta from "./pages/Meta.jsx";
 
 export default function App() {
   const location = useLocation();
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/start" element={<Start />} />
-          <Route path="/meta" element={<Meta />} />
-      </Routes>
-    </AnimatePresence>
+    <Routes location={location} key={location.pathname}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/start" element={<Start />} />
+      <Route path="/meta" element={<Meta />} />
+    </Routes>
   );
 }
